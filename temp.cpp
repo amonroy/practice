e6d45439ae8b701f38e028855fb0aed09b746ab3
Anais Monroy-Eklund <monroyanais@gmail.com>
2016-05-26 12:10:25 -0400

    Still just practicing all the things

---
+++ temp.cpp
@@ -0,0 +1,15 @@
+// This is a new file called temp.cpp
+// C-x C-f : Opens a file, asks for the file name. If it cannot find the file, creates the file.
+// C-x C-s : save the file without a prompt
+// C-x s   : saves all files with a prompt
+// C-s C-w : Saves the file with a different name. Asks you for the name.
+//
+// Everytime you save a file, Emacs creates another file with the name "filename~".
+// This tilde(~) file is the previous version of the file. It will be in the same dir.
+// Also, Emacs auto-saves everything you tupe to file with the name "#filename#".
+// If you quit Emacs without saving, you can this this auto-save file. Let's do that.
+//C-x C-c: Quits Emacs.
+//M-x : recovers the file.
+// _______________________
+// C-k : Kills/deletes the whole line, puts it into the clipboard.// C-k : Kills/deletes the whole line, puts it into the clipboard.// C-k : Kills/deletes the whole line, puts it into the clipboard.
+// C-y : pastes whatever is in the clipboard at the cursor.
